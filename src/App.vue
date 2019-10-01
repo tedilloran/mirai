312c8282130a9e6bef40dc074d840fbf18c1e20c
Trent Edilloran <trent@robotcache.com>
2019-09-30 18:57:04 -0700

    Some initial set up

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,30 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header></Header>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Header
   }
 }
 </script>
 
-<style>
+<style lang='scss'>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  background-color: $color-primary;
+  height: 100vh;
+  z-index: 0;
+  width: 100%;
+  position: relative;
+  box-sizing: border-box;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="header">
+    </div>
+</template>
+
+<script>
+var Header = {
+    name: 'Header',
+
+};
+export default Header;
+</script>
+
+<style scoped lang="scss">
+.header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 50rem;
+    background-color: rgba($color-black, 0.7);
+    z-index: 1;
+}
+</style>
