aa572566b0ef0a313d4b43459596b3f7cfd1da20
Trent Edilloran <trent.edilloran@gmail.com>
2020-05-16 21:59:28 -0700

    Formatting

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,11 @@
 module.exports = {
-    css: {
-        loaderOptions: {
-            sass: {
-                prependData: `
-                    @import "@/styles/main.scss";
-                `
-            }
-        }
+  css: {
+    loaderOptions: {
+      sass: {
+        prependData: `
+          @import "@/styles/main.scss";
+        `
+      }
     }
+  }
 };
